6dddc8ed89fd263da5dca62811c84db27f83bf02
FDSRashid <77756397+FDSRashid@users.noreply.github.com>
2023-09-16 21:06:51 -0700

    Update run_camel_data.sh

--- run_camel_data.sh
+++ run_camel_data.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Specify the path to the datasets folder (change this path as needed)
-datasets_folder="/path/to/your/datasets/camel_tools"
+# Default datasets folder path (current working directory)
+default_datasets_folder="$PWD"
+
+# Use the provided folder path or the default one
+datasets_folder="${1:-$default_datasets_folder}"
 
 # Create the datasets folder if it doesn't exist
-mkdir -p "$datasets_folder"
+mkdir -p "$datasets_folder/camel_tools"
 
 # Set the environment variable for camel-tools to point to the datasets folder
-export CAMELTOOLS_DATA="$datasets_folder"
+export CAMEL_TOOLS_DATA="$datasets_folder/camel_tools"
 
 # Install camel-tools packages
 camel_data -i all
 
-# Add more camel_data commands as needed
-# For example:
-# camel_data command1 arg1 arg2
-# camel_data command2 arg3
