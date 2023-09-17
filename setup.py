2c4afa81209ef7f285610486884a0b6ecc30fb55
FDSRashid <77756397+FDSRashid@users.noreply.github.com>
2023-09-16 19:53:42 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Use with Discretion, made very recently.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='semantic_similarity_arabic',
+    version='1.1.2',
+    author = "Ferdaws Rashid",
+    packages=find_packages(),
+    install_requires=[
+        # Include any additional dependencies here
+        'numpy>=1.18.0',
+        'torch>=2.0.1',
+        'faiss-cpu>=1.7.4',
+        'camel-tools>=1.5.2',
+        'transformers>=4.33.1'
+    ],
+)
