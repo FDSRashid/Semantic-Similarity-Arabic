5f0bddc38b8050e9134e8079c0d375b6a2b0313a
Ferdaws <rashidferdaws1@gmail.com>
2023-09-25 21:55:19 -0700

    Added requirements to setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='semantic_similarity_arabic',
     version='1.1.2',
-    author = "Ferdaws Rashid",
+    author="Ferdaws Rashid",
     packages=find_packages(),
     install_requires=[
         # Include any additional dependencies here
@@ -15,7 +15,17 @@ setup(
         'torch>=1.9.1',
         'faiss-cpu>=1.7.4',
         'camel-tools>=1.5.2',
-        'transformers>=4.30.2'
+        'transformers>=4.30.2',
+        'scikit-learn',
+        'pytest',
+        'datasets',
+        'umap-learn',
+        'scipy'
     ],
     python_requires='>=3.7'
 )
+
+
+
+
+
