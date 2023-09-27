df2624a53ac2ec7d7ba1c91bbec8a001cae0d97e
FDSRashid <77756397+FDSRashid@users.noreply.github.com>
2023-09-27 09:52:51 -0700

    Update setup_datasets.py

--- setup_datasets.py
+++ setup_datasets.py
@@ -5,9 +5,5 @@ def setup_datasets():
     # Download datasets using camel_data
     subprocess.run(["camel_data", "-i", "all"])
 
-    # Set the CAMELTOOLS_DATA environment variable
-    os.environ["CAMELTOOLS_DATA"] = "/root/.camel_tools"
-
-
 if __name__ == "__main__":
     setup_datasets()
