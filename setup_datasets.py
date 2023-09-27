922991cf92979527d2880760d2857b68c1c8ad81
FDSRashid <77756397+FDSRashid@users.noreply.github.com>
2023-09-27 09:17:02 -0700

    Update setup_datasets.py

--- setup_datasets.py
+++ setup_datasets.py
@@ -6,7 +6,9 @@ def setup_datasets():
     subprocess.run(["camel_data", "-i", "all"])
 
     # Set the CAMELTOOLS_DATA environment variable
-    os.environ["CAMELTOOLS_DATA"] = "~/.camel_tools"
+    data_path = os.path.expanduser("~/.camel_tools")  # Expands '~' to the user's home directory
+    os.environ["CAMELTOOLS_DATA"] = data_path
+
 
 if __name__ == "__main__":
     setup_datasets()
