908b956c992e311e1a96b13a25dbc1fea44a3503
Ferdaws <rashidferdaws1@gmail.com>
2023-09-27 08:24:18 -0700

    User download automation option added

--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
-"""
-Use with Discretion, made very recently.
-"""
+
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import subprocess
+
+
+
+
+class CustomInstall(install):
+    user_options = install.user_options + [
+        ('install-datasets', None, 'Install camel-tools datasets and set environment variable to the location of the download.'),
+    ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.custom_install = False
+
+    def finalize_options(self):
+        install.finalize_options(self)
+
+    def run(self):
+        # Run the default installation first
+        install.run(self)
+        
+        if self.custom_install:
+            # Download datasets using camel_data
+            subprocess.run(["camel_data", "-i", "all"])
+
+            # Set the CAMELTOOLS_DATA environment variable
+            os.environ["CAMELTOOLS_DATA"] = "~/.camel_tools"
+
+
 
 setup(
     name='semantic_similarity_arabic',
-    version='1.1.2',
+    version='1.1.0',
     author="Ferdaws Rashid",
     packages=find_packages(),
     install_requires=[
@@ -22,7 +51,8 @@ setup(
         'umap-learn',
         'scipy'
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.7',
+    cmdclass={'install': CustomInstall},
 )
 
 
