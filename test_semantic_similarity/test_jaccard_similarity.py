2dbdd1f89a2e4cc1948ca319aa6a7479089aa9cb
Ferdaws <rashidferdaws1@gmail.com>
2023-09-26 22:53:12 -0700

    Attempting download through yml

---
+++ test_semantic_similarity/test_jaccard_similarity.py
@@ -0,0 +1,27 @@
+import pytest
+from jaccardsimilarity.jaccard_similarity  import JaccardSimilarity
+import os
+
+# Fixture to create an instance of JaccardSimilarity for testing
+@pytest.fixture
+def jaccard_similarity_instance():
+    
+    yield JaccardSimilarity('calima-msa-r13')
+    
+
+# Test the preprocess method
+def test_preprocess(jaccard_similarity_instance):
+    input_sentence = " فَسَمِعَ رَجُلا "
+    expected_output = " فسمع رجلا "
+    result = jaccard_similarity_instance.preprocess(input_sentence)
+    assert result == expected_output
+
+def test_tokenizer(jaccard_similarity_instance):
+    input_sentence = " فسمع رجلا "
+    expected_output = ['ف+', 'سمع', 'رجلا']
+    result = jaccard_similarity_instance.tokenize(input_sentence)
+    assert result == expected_output
+
+
+if __name__ == "__main__":
+    pytest.main()
